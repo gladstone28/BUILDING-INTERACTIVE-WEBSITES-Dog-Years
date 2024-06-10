ac2e1aae56de9b3b5a05674944a7e547ba55573e
Gladstone Rose <gladstonerose@hotmail.com>
2024-06-10 17:28:10 -0500

    modified files to get interactivity

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const myAge = 13;
-// Create variable with value of 2
-let earlyYears = 2;
-earlyYears = earlyYears * 10.5;
+document.getElementById('ageForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const humanAge = parseInt(document.getElementById('humanAge').value);
+    
+    if (!isNaN(humanAge)) {
+        let earlyYears = 2;
+        earlyYears = earlyYears * 10.5;
+        
+        let laterYears = humanAge - 2;
+        laterYears = laterYears > 0 ? laterYears * 4 : 0;
 
-let laterYears = myAge - 2;
-laterYears *= 4;
-
-console.log(earlyYears);
-console.log(laterYears);
-
-let myAgeInDogYears = earlyYears + laterYears;
-console.log(myAgeInDogYears);
-
-let myName = "Pepper".toLowerCase();
-
-console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
-
-document.getElementById('output').textContent = `My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`;
+        const dogAge = earlyYears + laterYears;
+        
+        document.getElementById('output').textContent = `You are ${dogAge} years old in dog years.`;
+    } else {
+        document.getElementById('output').textContent = 'Please enter a valid age.';
+    }
+});
