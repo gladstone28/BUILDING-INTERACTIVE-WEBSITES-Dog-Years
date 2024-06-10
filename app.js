ad6fa072f8fa7e965ff4f934d1d8d82748627112
Gladstone Rose <gladstonerose@hotmail.com>
2024-06-10 17:12:18 -0500

    modified app.js and added index.html and styles.css

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const myAge = 13;
-//create variable with value of 2;
+// Create variable with value of 2
 let earlyYears = 2;
 earlyYears = earlyYears * 10.5;
-laterYears = myAge - 2;
+
+let laterYears = myAge - 2;
 laterYears *= 4;
+
 console.log(earlyYears);
 console.log(laterYears);
 
 let myAgeInDogYears = earlyYears + laterYears;
 console.log(myAgeInDogYears);
+
 let myName = "Pepper".toLowerCase();
 
-console.log(
-  `My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`
-);
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+document.getElementById('output').textContent = `My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`;
